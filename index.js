46997bb26edfb5dd18ff7200ed32a4e4fb6a2311
JACK-HRUSHI <hrushikeshjagtap2002@gmail.com>
2022-01-10 11:43:52 +0530

    Version 1

---
+++ index.js
@@ -0,0 +1,102 @@
+console.log("This is index.js");
+
+// Constructor
+
+function Book(name, author, type) {
+  this.name = name;
+  this.author = author;
+  this.type = type;
+}
+
+// Display Constructor
+
+function Display() {}
+
+// Add methods to display prototype
+
+Display.prototype.add = function (book) {
+  console.log("Adding to UI");
+  tableBody = document.getElementById("tableBody");
+  let uiString = `<tr>
+                    <td>${book.name}</td>
+                    <td>${book.author}</td>
+                    <td>${book.type}</td>
+                  </tr>`;
+
+  tableBody.innerHTML += uiString;
+};
+
+// Display to clear function
+
+Display.prototype.Clear = function () {
+  let libraryForm = document.getElementById("libraryForm");
+  libraryForm.reset();
+};
+
+// Display to validate function
+
+Display.prototype.validate = function (book) {
+  let libraryForm = document.getElementById("libraryForm");
+  if (book.name.length < 2 || book.author.length < 2) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+Display.prototype.show = function (type, displaymessage) {
+  let message = document.getElementById("message");
+  message.innerHTML = `<div class="alert alert-${type} fade show" role="alert">
+                          <strong>Message:</strong>  ${displaymessage}
+                          <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>`;
+
+  // Message will automatically disappears after 2000s (2ms)
+
+  setTimeout(() => {
+    message.innerHTML = "";
+  }, 2000);
+};
+
+// Add SUBMIT event listner to libraryForm
+
+let libraryForm = document.getElementById("libraryForm");
+libraryForm.addEventListener("submit", libraryFormSubmit);
+
+function libraryFormSubmit(e) {
+  console.log("You have submitted Library Form");
+
+  let name = document.getElementById("bookName").value;
+  let author = document.getElementById("author").value;
+  let type;
+
+  let fiction = document.getElementById("fiction");
+  let programming = document.getElementById("programming");
+  let cooking = document.getElementById("cooking");
+
+  if (fiction.checked) {
+    type = fiction.value;
+  } else if (programming.checked) {
+    type = programming.value;
+  } else if (cooking.checked) {
+    type = cooking.value;
+  }
+
+  let book = new Book(name, author, type);
+  console.log(book);
+
+  let display = new Display();
+
+  if (display.validate(book)) {
+    display.add(book);
+    display.Clear();
+    display.show("success", "Your book has been successfully added.");
+  } else {
+    //Error to the user
+    display.show("danger", "Sorry you cannot add your book.");
+  }
+
+  e.preventDefault(); // To prevent reload functionality of form
+}
+
+// 48:45
